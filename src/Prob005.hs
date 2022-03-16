f0c9967fd7777882a07935133aad19e3a7cc3b65
toni <tblaslov@expediagroup.com>
2022-03-16 19:03:32 +0000

    wip prob5

--- src/Prob005.hs
+++ src/Prob005.hs
@@ -1,7 +1,23 @@
-module Prob005 where
+module Prob005 (primeFactors, sol005, comb) where
 
- import Data.IntMap (IntMap)
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
 
 
-sol005 :: Integer -> Integer
+sol005 :: Int -> Int
 sol005 n = 1
+
+comb :: [Int] -> [Int] -> [Int]
+comb l1 l2 | length l1 > length l2 = l1
+comb l1 l2                         = l2
+
+
+primeFactors :: Int -> [Int]
+primeFactors 1 = [1]
+primeFactors n = pf n 2
+  where
+    pf :: Int -> Int -> [Int]
+    pf n k | n == 1       = []
+    pf n k | k > n        = []
+    pf n k | mod n k == 0 = k : pf (div n k) k
+    pf n k                = pf n (k+1)
--- test/Prob005Spec.hs
+++ test/Prob005Spec.hs
@@ -1,10 +1,37 @@
 module Prob005Spec where
 
-import Test.Hspec ( describe, it, Spec )
-import Prob005 
+
+import Test.Hspec ( describe, it, Spec, shouldBe )
+import Prob005 (primeFactors, comb, sol005)
 
 spec :: Spec
 spec = do
   describe "Prob005" $ do
-    it "can calculate palindromes of ints" $ do
-      sol005 1 == 1
+
+    it "can calculate prime factors of 1" $ do
+      shouldBe (primeFactors 1) [1]
+
+    it "can calculate prime factors of 5" $ do
+      shouldBe (primeFactors 5) [5]
+
+    it "can calculate prime factors of 10" $ do
+      shouldBe (primeFactors 10) [2,5]
+
+    it "can calculate prime factors of 8" $ do
+      shouldBe (primeFactors 8) [2,2,2]
+
+    it "can combine lists of different lengths" $ do
+      shouldBe (comb [1] [1, 1]) [1, 1]
+
+    it "can combine lists of same lengths" $ do
+      shouldBe (comb [1, 1] [1, 1]) [1, 1]
+
+    it "can combine list with empty list" $ do
+      shouldBe (comb [] [1, 1]) [1, 1]
+    
+    it "can combine two empty lists" $ do
+      shouldBe (comb [] []) []
+
+    it "can calculate the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder" $ do
+      shouldBe (sol005 10) 2520
+
